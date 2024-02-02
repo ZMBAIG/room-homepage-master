0f570ed84b0466a2ecdfe70b94db8c641cc1c382
Zafar <baigzafar@gmail.com>
2024-02-02 21:52:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+// ================================================
+
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
